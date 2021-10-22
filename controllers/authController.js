2534e9a818495e123f27259ab30e2ce497943d15
DaveDevPerry <daveperry.log@gmail.com>
2021-10-23 00:26:27 +0100

    login in successful

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,7 +50,11 @@ module.exports.signup_post = async (req, res) => {
 		const user = await User.create({ email, password });
 		const token = createToken(user._id);
 		// place inside cookie
-		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			secure: true,
+			maxAge: maxAge * 1000,
+		});
 		res.status(201).json({ user: user._id });
 	} catch (err) {
 		const errors = handleErrors(err);
@@ -66,7 +70,11 @@ module.exports.login_post = async (req, res) => {
 		const user = await User.login(email, password);
 		const token = createToken(user._id);
 		// place inside cookie
-		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			secure: true,
+			maxAge: maxAge * 1000,
+		});
 		res.status(200).json({ user: user._id });
 	} catch (err) {
 		const errors = handleErrors(err);
